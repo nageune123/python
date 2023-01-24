3e8c9a79dda6508310513ce5ae0cb7e37ba3c2e7
kimjunyeon <ziririsky8@naver.com>
2023-01-24 22:56:50 +0900

    python1
    1

---
+++ python1.py
@@ -0,0 +1,14 @@
+a="Life si too short, You need Python"
+b=a[0]+a[1]+a[2]+a[3]
+a[0:4]
+#0<=a<3
+#a[시작번호:끝번호]
+print(b)
+number =10
+day= "three"
+print("I ate %d apples. so I was sick for %s days." %(number,day))
+print("I ate {0} apples. so I was sick for {1} days.".format(number,day))
+
+a="hobby"
+print(a.count('b'))
+
